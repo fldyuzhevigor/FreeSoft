c6188fe96d599127ca356aaa38781de31eb7c304
Microsoft <Microsoft@DESKTOP-SP45540>
2016-09-19 08:25:31 +0300

    Add LoggetIn Action

--- FreeSoft/Controllers/AccountController.cs
+++ FreeSoft/Controllers/AccountController.cs
@@ -45,6 +45,25 @@ namespace FreeSoft.Controllers
             using (OurDbContext db = new OurDbContext())
             {
                 var usr = db.UserAccounts.Single(u => u.Username == userAccount.Username && u.Password == userAccount.Password);
+                if (usr != null)
+                {
+                    Session["UserId"] = usr.UserId.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("LoggetIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "UserName or Password is wrong.");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LoggetIn()
+        {
+            if (Session["UserId"]!= null)
+            {
+                
             }
             return View();
         }
